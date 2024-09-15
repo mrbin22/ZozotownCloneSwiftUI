c26a9e9bea9ab6910abde77f8017afb07b9fffd4
mrbin22 <23cm0122@jec.ac.jp>
2024-09-15 22:07:30 +0900

    implement simultaneous gesture to do control scroll view when user scroll this excute and hidden or visible tabbar

---
+++ ZozotownClone/Views/Home/SimultaneousGesture.swift
@@ -0,0 +1,18 @@
+//
+//  SimultaneousGesture.swift
+//  ZozotownClone
+//
+//  Created by cmStudent on 2024/09/15.
+//
+
+import SwiftUI
+
+struct SimultaneousGesture: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SimultaneousGesture()
+}
