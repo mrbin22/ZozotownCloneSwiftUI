6724cb9eebe9e0a381b50ddf49f92f5f123c1764
mrbin22 <23cm0122@jec.ac.jp>
2024-09-16 22:56:44 +0900

    add login, sign up view, etc...

---
+++ ZozotownClone/Views/Welcome/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  ZozotownClone
+//
+//  Created by cmStudent on 2024/09/16.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
