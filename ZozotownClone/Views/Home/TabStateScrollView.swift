f89beae6a608e9c179d13afce586ae7a3d67556a
mrbin22 <23cm0122@jec.ac.jp>
2024-09-16 17:26:12 +0900

    add cartview and active sheet

---
+++ ZozotownClone/Views/Home/TabStateScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  TabStateScrollView.swift
+//  ZozotownClone
+//
+//  Created by cmStudent on 2024/09/15.
+//
+
+import SwiftUI
+
+struct TabStateScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabStateScrollView()
+}
---
+++ ZozotownClone/Views/Product/SelectSizesView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectSizesView.swift
+//  ZozotownClone
+//
+//  Created by cmStudent on 2024/09/16.
+//
+
+import SwiftUI
+
+struct SelectSizesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectSizesView()
+}
