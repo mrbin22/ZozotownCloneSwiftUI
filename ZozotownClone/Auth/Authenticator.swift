855aa9a0dfff1b64e962310e8627c30a8e723c70
mrbin22 <23cm0122@jec.ac.jp>
2024-09-19 23:44:32 +0900

    update features

---
+++ ZozotownClone/Auth/Authenticator.swift
@@ -0,0 +1,8 @@
+//
+//  Authenticator.swift
+//  ZozotownClone
+//
+//  Created by cmStudent on 2024/09/17.
+//
+
+import Foundation
---
+++ ZozotownClone/Views/Welcome/SignUpWithEmailView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpWithEmailView.swift
+//  ZozotownClone
+//
+//  Created by cmStudent on 2024/09/17.
+//
+
+import SwiftUI
+
+struct SignUpWithEmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpWithEmailView()
+}
